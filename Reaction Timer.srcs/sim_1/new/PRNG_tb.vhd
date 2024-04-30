46c332b09e964265fe3dbd65b7b9badc6e61d0c8
Yeni Choi <ych227@uclive.ac.nz>
2024-04-30 13:09:45 +1200

    Created tb for PRNG

---
+++ Reaction Timer.srcs/sim_1/new/PRNG_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 30.04.2024 13:08:56
+-- Design Name: 
+-- Module Name: PRNG_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PRNG_tb is
+--  Port ( );
+end PRNG_tb;
+
+architecture Behavioral of PRNG_tb is
+
+begin
+
+
+end Behavioral;
